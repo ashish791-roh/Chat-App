320aa6dc5c3b77827fa4ff904c2a2ca0c27cfce8
ShiwaniBhandari004 <shiwanibhandari967@gmail.com>
2026-03-31 20:47:15 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNcbtYKWmjFQqMam-1l7QxAN7NW5mDKCA",
-  authDomain: "chat-app-fa051.firebaseapp.com",
-  databaseURL: "https://chat-app-fa051-default-rtdb.firebaseio.com",
-  projectId: "chat-app-fa051",
-  storageBucket: "chat-app-fa051.firebasestorage.app",
-  messagingSenderId: "420985704789",
-  appId: "1:420985704789:web:4db455607bd089f3fb9fdc",
-  measurementId: "G-6NHEHEWZZX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
