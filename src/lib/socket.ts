78a4c7d6d33795b27c32fabeb9d29dac32496e84
ShiwaniBhandari004 <shiwanibhandari967@gmail.com>
2026-03-31 08:40:27 +0530

    Update socket.ts

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,8 +1,7 @@
 import { io } from 'socket.io-client';
 
-// Replace with backend URL later
 const SOCKET_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000';
 
 export const socket = io(SOCKET_URL, {
-  autoConnect: false, // Don't connect until the user logs in
+  autoConnect: false,
 });
